1e2cc1c5cd5af59ca3b7a5ece1fea800cd1aa99c
AntonAngeles <77523922+AntonAngeles@users.noreply.github.com>
2024-11-27 16:07:38 -0700

    Implemented forward delete in layer 2 where shift is

--- keymaps/AntonAngeles/keymap.c
+++ keymaps/AntonAngeles/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_SECOND] = LAYOUT_ortho_4x12(
         KC_TRNS,   KC_7,    KC_8,    KC_9,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    	KC_9,    	KC_0,  		KC_BSPC,
         KC_TRNS,   KC_4,    KC_5,    KC_6,    KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,   KC_LBRC,   	KC_RBRC,  	KC_BSLS,
-        KC_LSFT,   KC_1,    KC_2,    KC_3,    KC_0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   	KC_TRNS,   	KC_TRNS,
+        KC_LSFT,   KC_1,    KC_2,    KC_3,    KC_0, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,   	KC_TRNS,   	KC_DEL,
         KC_PGUP,   KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,	KC_TRNS, KC_TRNS, KC_MNXT,  KC_VOLD,  	KC_VOLU,    KC_MPLY
     )
 };
